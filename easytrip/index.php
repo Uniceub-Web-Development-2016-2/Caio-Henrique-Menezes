78974f5049eb25b10f6250032c28dd20ecb53f99
caiohnrq <caio_henrique.16@hotmail.com>
2016-10-18 20:43:40 -0200

    updating folders and some methods

---
+++ easytrip/index.php
@@ -0,0 +1,32 @@
+<?php
+include('../easytrip/control/request_controller.php');
+
+$controller = new RequestController();
+$dados = $controller->execute();
+	 
+$arr = utf8_converter($dados);
+	 
+function utf8_converter($array){
+	    array_walk_recursive($array, function(&$item, $key){
+		if(!mb_detect_encoding($item, 'utf-8', true)){
+		        $item = utf8_encode($item);
+		}
+	    });
+	 
+	    return $array;
+	}
+
+print_r($arr);
+
+
+
+
+
+
+
+
+
+
+
+
+//print_r(utf8_encode($controller->execute()));
